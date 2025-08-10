f733dfd3e6f5f78192204d1782341a5ed8fb7444
Julian-D-Leal <leal.julian@correounivalle.edu.co>
2025-08-10 01:42:20 -0500

    add deploy workflow

--- todos-app/src/App.jsx
+++ todos-app/src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/Header'
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Footer from './components/Footer';
-import Card from './components/Card';
+
 
 function App() {
   return (
